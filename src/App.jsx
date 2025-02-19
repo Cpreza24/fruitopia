9000e3085b32153a2e7d0aeb4dc953a5d7e6dfaf
Cesar Preza <cpreza93@yahoo.com>
2025-02-18 19:59:27 -0800

    adds Routes for home about and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
+import { Route, Routes } from 'react-router';
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Fruits from './components/Fruits/Fruits';
 import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Fruits />
+      <Routes>
+        <Route path='/' element={<Fruits />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './Navbar.css';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
-  return <div className='navbar-logo'>Fruitopia</div>;
+  return (
+    <div className='navbar'>
+      <div className='navbar-logo'>Fruitopia</div>
+      <div className='navbar-menu'>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/contact'>Contact</NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
